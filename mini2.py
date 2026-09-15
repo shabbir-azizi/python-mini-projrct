d8345e70cf87b5c9932bfc75d8cbae06f4844e86
shabbir <azizishabbir355@gmail.com>
2026-09-15 22:55:40 +0500

    working on mini project in python macking project

---
+++ mini2.py
@@ -0,0 +1,7 @@
+# name=(input("enter your name: "))
+# print( input("welcome to our coding"))
+# input ("enter yor good name")
+
+print("welcome to codeing")
+ourinput= input("enter our good name: ")
+print("hi", ourinput ,"how are you ?")
