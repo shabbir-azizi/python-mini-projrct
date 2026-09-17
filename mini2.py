fdf830fd596135404921d1e1142620267e0eeb96
shabbir <azizishabbir355@gmail.com>
2026-09-17 20:37:52 +0500

    working on mini project in python macking project

--- mini2.py
+++ mini2.py
@@ -13,3 +13,11 @@ p3 =int (input("enter the prise od third product: "))
 total = p1 + p2 + p3
 
 print("your total billing is: ", total)
+# name=(input("enter your name: "))
+# print( input("welcome to our coding"))
+# input ("enter yor good name")
+
+print("welcome to codeing")
+ourinput= input("enter our good name: ")
+print("hi", ourinput ,"how are you ?")
+
