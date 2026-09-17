6c503fcb25d73ae0b43845c69f8729695e782677
shabbir <azizishabbir355@gmail.com>
2026-09-17 20:38:11 +0500

    working on mini project in python macking project

--- mini2.py
+++ mini2.py
@@ -21,3 +21,10 @@ print("welcome to codeing")
 ourinput= input("enter our good name: ")
 print("hi", ourinput ,"how are you ?")
 
+p1 =int(input("enter the prise of frist product: "))
+p2 =int (input("enter the prise od second product: "))
+p3 =int (input("enter the prise od third product: "))
+
+total = p1 + p2 + p3
+
+print("your total billing is: ", total)
