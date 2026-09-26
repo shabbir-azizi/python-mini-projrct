89073fb4b7ef317fd3fcfb32f9b2a2ef87688c45
shabbir <azizishabbir355@gmail.com>
2026-09-26 17:28:37 +0500

    working on mini project in python macking project

---
+++ abc.py
@@ -0,0 +1,19 @@
+abs_1 = 57
+abs_2 = 13
+asa_1 = 6.4
+asa_2 = 13.0
+
+
+
+print('Integer Addition:', abs_1 + abs_2)
+print('Float Addition:', asa_1 + asa_2) 
+
+print('Int Subtraction:', abs_1 - abs_2) 
+print('Float Subtraction:',  asa_2 - asa_1) 
+
+
+print('Int Multiplication:', abs_1 * abs_2) 
+print('Float Multiplication:', asa_2 * asa_1) 
+
+print('Division:', abs_1 / abs_2) 
+print('Float Division:', asa_2 / asa_1) 
